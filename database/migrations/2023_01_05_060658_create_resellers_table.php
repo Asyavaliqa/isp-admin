29fc875ed4b962b205defd882409c04fb319a6ac
Ashary Vermaysha <asharyver13@gmail.com>
2023-01-05 13:13:03 +0700

    change foreign id

--- database/migrations/2023_01_05_060658_create_resellers_table.php
+++ database/migrations/2023_01_05_060658_create_resellers_table.php
@@ -10,10 +10,8 @@ return new class extends Migration
     {
         Schema::create('resellers', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('user_id');
-            $table->timestamps();
-
             $table->foreignId('user_id')->constrained();
+            $table->timestamps();
         });
     }
 
--- database/migrations/2023_01_05_060918_create_clients_table.php
+++ database/migrations/2023_01_05_060918_create_clients_table.php
@@ -10,14 +10,10 @@ return new class extends Migration
     {
         Schema::create('clients', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('user_id');
-            $table->unsignedBigInteger('bandwidth_id');
-            $table->unsignedBigInteger('reseller_id');
-            $table->timestamps();
-
             $table->foreignId('user_id')->constrained();
             $table->foreignId('bandwidth_id')->constrained();
             $table->foreignId('reseller_id')->constrained();
+            $table->timestamps();
         });
     }
 
