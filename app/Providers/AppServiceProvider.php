ba523b29e7f85440af58115e934b4ae12fca8b92
Ashary Vermaysha <asharyver13@gmail.com>
2023-01-09 20:18:43 +0700

    enable auto discovery

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,10 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        // Load IDE Helper when in local development
-        if ($this->app->isLocal() && class_exists(App\Providers\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class)) {
-            $this->app->register(Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
-        }
+        //
     }
 
     /**
