33d47d744f4be3ceb9c3a035c049d28bd48b80a9
Ashary Vermaysha <asharyver13@gmail.com>
2023-01-10 01:44:10 +0700

    add missing relationship

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Client extends Model
 {
     use HasFactory;
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function bandwidth()
+    {
+        return $this->belongsTo(Bandwidth::class);
+    }
+
+    public function reseller()
+    {
+        return $this->belongsTo(Reseller::class);
+    }
 }
