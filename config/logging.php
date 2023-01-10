818321ce5c5e0a5d22305062374f9491ef63d03e
Ashary Vermaysha <asharyver13@gmail.com>
2023-01-10 22:12:38 +0700

    change logging stack

--- config/logging.php
+++ config/logging.php
@@ -53,7 +53,7 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['single'],
+            'channels' => ['daily', 'syslog', 'errorlog'],
             'ignore_exceptions' => false,
         ],
 
