4c8c2d2fad3ff51aaa08ad6700797615a22d5999
Ashary Vermaysha <asharyver13@gmail.com>
2023-01-10 19:31:35 +0700

    Update views

--- resources/views/layouts/pages.blade.php
+++ resources/views/layouts/pages.blade.php
@@ -1,21 +1,31 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
-<head>
+<html lang="en">
+  <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>{{ $pageTitle }} | {{ config('app.name') }}</title>
+    <title>@yield('pageTitle') | {{ config('app.name') }}</title>
 
-    @vite(['resources/scss/vendors/simplebar.scss', 'resources/scss/style.scss'])
-</head>
+    <!-- Vendors styles-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/simplebar@5.3.9/dist/simplebar.min.css">
+    <link rel="stylesheet" href="{{ mix('css/simplebar.css') }}">
+    <!-- Main styles for this application-->
+    <link href="{{ mix('css/style.css') }}" rel="stylesheet">
 
-<body>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@coreui/chartjs@3.0.0/dist/css/coreui-chartjs.min.css">
+    @yield('stylesheet')
+  </head>
+  <body>
+
     @yield('content')
 
-    @vite(['resources/js/app.js'])
-    <script></script>
+    <!-- CoreUI and necessary plugins-->
+    <script src="https://cdn.jsdelivr.net/npm/@coreui/coreui@4.2.6/dist/js/coreui.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/simplebar@5.3.9/dist/simplebar.min.js"></script>
+    <!-- Plugins and scripts required by this view-->
+    @yield('script')
+    <script>
+    </script>
 
-</body>
-
+  </body>
 </html>
