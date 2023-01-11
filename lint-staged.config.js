d79d81482d41bf74a7f37c081efbe41f7f9f2977
Ashary Vermaysha <asharyver13@gmail.com>
2023-01-11 08:29:27 +0700

    Format code

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -5,7 +5,10 @@ module.exports = {
     // from `files` filter those _NOT_ matching `*test.js`
     const match = micromatch.not(
         files, [
-            '.history', '_ide_helper_models.php', '_ide_helper.php', '.phpstorm.meta.php'
+            '*.history',
+            '*_ide_helper_models.php',
+            '*_ide_helper.php',
+            '*.phpstorm.meta.php'
         ])
     return `./vendor/bin/pint --config ./pint.json ${match.join(' ')}`
   },
