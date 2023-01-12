ce2f409f42972f4a481d8160690317c25268c3d5
Ashary Vermaysha <asharyver13@gmail.com>
2023-01-12 13:11:18 +0700

    create reseller controller

---
+++ app/Http/Controllers/Admin/ResellerController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ResellerController extends Controller
+{
+    public function index(Request $request)
+    {
+        return view('pages.admin.reseller.index', [
+            'title' => 'Reseller',
+        ]);
+    }
+}
