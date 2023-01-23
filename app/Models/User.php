503e323648cc42425dc5237817aa940091d9b82a
Ashary Vermaysha <asharyver13@gmail.com>
2023-01-23 13:07:33 +0700

    change fillable column

--- app/Models/User.php
+++ app/Models/User.php
@@ -26,7 +26,7 @@ class User extends Authenticatable
         'username',
         'email',
         'password',
-        'birth',
+        'birthday',
         'gender',
         'address',
         'photo',
