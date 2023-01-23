d9e12787ede1a8b038eb392c0194c54e1918ab6d
iamRAF <renoxf11@gmail.com>
2023-01-23 14:24:25 +0700

    update reseller pages

---
+++ resources/views/pages/reseller/client/index.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.dashboard')
+
+@section('pageTitle')
+    {{ $title ?? 'Pelanggan' }}
+@endsection
+
+@section('content')
+<div class="container-lg">
+    <div class="row g-0">
+        <div class="card">
+            <div class="card-header">
+                <strong>Pelanggan</strong>
+            </div>
+            <div class="card-body py-4">
+                <div class="d-flex justify-content-between mb-3">
+                    <div class="px-3">
+                        <input type="text" class="form-control" placeholder="Search ..">
+                    </div>
+                </div>
+                <div class="table-responsive px-3">
+                    <table class="table table-hover align-middle custom-table">
+                        <thead>
+                            <tr>
+                              <th scope="col">Nama Pelanggan</th>
+                              <th scope="col">Paket</th>
+                              <th scope="col">No.Telp</th>
+                              <th scope="col">Alamat</th>
+                              <th scope="col">Status</th>
+                            </tr>
+                          </thead>
+                          <tbody class="table-group-divider">
+                          </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
