fbfa21223674c3a4e3b206524ecb7e650cb0e0f7
Ashary Vermaysha <asharyver13@gmail.com>
2023-01-30 16:42:58 +0700

    bandwidth list controller

--- app/Http/Controllers/Reseller/BandwidthController.php
+++ app/Http/Controllers/Reseller/BandwidthController.php
@@ -3,14 +3,27 @@
 namespace App\Http\Controllers\Reseller;
 
 use App\Http\Controllers\Controller;
+use App\Models\Bandwidth;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class BandwidthController extends Controller
 {
+    /**
+     * Show table of available bandwidth
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
     public function index(Request $request)
     {
+        $bandwidths = Bandwidth::whereHas('reseller', function ($q) {
+            $q->where('user_id', Auth::id());
+        })->orderBy('id', 'desc')->paginate();
+
         return view('pages.reseller.bandwidth.index', [
-            'title' => 'Pelanggan',
+            'title' => 'Paket Internet',
+            'bandwidths' => $bandwidths,
         ]);
     }
 }
