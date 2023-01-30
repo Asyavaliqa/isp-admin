6c9ac4f9997f2ead358ed8d1d2a43d234597c19f
iamRAF <renoxf11@gmail.com>
2023-01-30 23:14:39 +0700

    add uptime server

--- resources/views/pages/admin/home.blade.php
+++ resources/views/pages/admin/home.blade.php
@@ -78,6 +78,24 @@
                     </div>
                 </div>
             </div>
+
+            <div class="col-sm-6 col-lg-3">
+                <div class="card mb-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="fs-4 fw-semibold">{{ $upTime }}</div>
+                            <div>Server Uptime</div>
+                            <div class="progress progress-thin my-2">
+                                <div class="progress-bar bg-success" role="progressbar"
+                                    style="width: 100%"
+                                    aria-valuenow="0" aria-valuemin="0"
+                                    aria-valuemax="100"></div>
+                            </div>
+                            {{-- <small class="text-medium-emphasis-inverse">Widget helper text</small> --}}
+                        </div>
+                    </div>
+                </div>
+            </div>
             {{-- /col --}}
             {{-- col --}}
             {{-- <div class="col-sm-6 col-lg-3">
@@ -101,12 +119,12 @@
             <div class="col-sm-6 col-lg-3">
                 <div class="card overflow-hidden">
                     <div class="card-body p-0 d-flex align-items-center">
-                        <div class="bg-primary text-white py-4 px-5 me-3">
+                        <div class="bg-primary text-white py-4 px-4 me-3">
                             <i class="icon icon-xl cil-people"></i>
                         </div>
                         <div>
                             <div class="fs-6 fw-semibold text-primary">{{ $userTotal }}</div>
-                            <div class="text-medium-emphasis text-uppercase fw-semibold small">Pengguna</div>
+                            <div class="text-medium-emphasis text-uppercase fw-semibold small">Akun Pengguna</div>
                         </div>
                     </div>
                 </div>
@@ -114,25 +132,12 @@
             <div class="col-sm-6 col-lg-3">
                 <div class="card overflow-hidden">
                     <div class="card-body p-0 d-flex align-items-center">
-                        <div class="bg-primary text-white py-4 px-5 me-3">
-                            <i class="icon icon-xl cil-people"></i>
-                        </div>
-                        <div>
-                            <div class="fs-6 fw-semibold text-primary">{{ $clientTotal }}</div>
-                            <div class="text-medium-emphasis text-uppercase fw-semibold small">Klien</div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-6 col-lg-3">
-                <div class="card overflow-hidden">
-                    <div class="card-body p-0 d-flex align-items-center">
-                        <div class="bg-primary text-white py-4 px-5 me-3">
+                        <div class="bg-primary text-white py-4 px-4 me-3">
                             <i class="icon icon-xl cil-people"></i>
                         </div>
                         <div>
                             <div class="fs-6 fw-semibold text-primary">{{ $mitraTotal }}</div>
-                            <div class="text-medium-emphasis text-uppercase fw-semibold small">Mitra</div>
+                            <div class="text-medium-emphasis text-uppercase fw-semibold small">Reseller AKTIF</div>
                         </div>
                     </div>
                 </div>
@@ -140,12 +145,25 @@
             <div class="col-sm-6 col-lg-3">
                 <div class="card overflow-hidden">
                     <div class="card-body p-0 d-flex align-items-center">
-                        <div class="bg-primary text-white py-4 px-5 me-3">
+                        <div class="bg-primary text-white py-4 px-4 me-3">
                             <i class="icon icon-xl cil-people"></i>
                         </div>
                         <div>
-                            <div class="fs-6 fw-semibold text-primary">$1.999,50</div>
-                            <div class="text-medium-emphasis text-uppercase fw-semibold small">Pendapatan</div>
+                            <div class="fs-6 fw-semibold text-primary">{{ $clientTotal }}</div>
+                            <div class="text-medium-emphasis text-uppercase fw-semibold small">Pelanggan RESELLER</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-lg-3">
+                <div class="card overflow-hidden">
+                    <div class="card-body p-0 d-flex align-items-center">
+                        <div class="bg-primary text-white py-4 px-4 me-3">
+                            <i class="icon icon-xl cil-people"></i>
+                        </div>
+                        <div>
+                            <div class="fs-6 fw-semibold text-primary">{{ $mitraNonaktif }}</div>
+                            <div class="text-medium-emphasis text-uppercase fw-semibold small">RESELLER NONAKTIF</div>
                         </div>
                     </div>
                 </div>
@@ -156,7 +174,7 @@
         <div class="row">
             <div class="col-md-12">
                 <div class="card mb-4">
-                    <div class="card-header text-center fw-semibold">Top Mitra Dengan Pelanggan Terbanyak</div>
+                    <div class="card-header text-center fw-semibold">Mitra Dengan Pelanggan Terbanyak</div>
                     <div class="card-body">
                         <div class="table-responsive">
                             <table class="table border mb-0">
