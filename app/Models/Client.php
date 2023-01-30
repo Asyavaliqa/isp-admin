b1c789260c4bf6522e913e53eba60c32b02ef709
Ashary Vermaysha <asharyver13@gmail.com>
2023-01-30 16:34:13 +0700

    add comment

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -12,16 +12,31 @@ class Client extends Model
 {
     use HasFactory;
 
+    /**
+     * Relation to User (Client account)
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function user()
     {
         return $this->belongsTo(User::class);
     }
 
+    /**
+     * Relation to bandwidth
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function bandwidth()
     {
         return $this->belongsTo(Bandwidth::class);
     }
 
+    /**
+     * Relation to reseller
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function reseller()
     {
         return $this->belongsTo(Reseller::class);
