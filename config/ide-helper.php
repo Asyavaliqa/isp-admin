e63a94e7fcca60bca28063629a322150f5a04298
Ashary Vermaysha <asharyver13@gmail.com>
2023-01-31 15:51:15 +0700

    IDE Helper update

--- config/ide-helper.php
+++ config/ide-helper.php
@@ -290,7 +290,7 @@ return [
     | or there is an import (use className) of a given class
     |
     */
-    'force_fqn' => false,
+    'force_fqn' => true,
 
     /*
     |--------------------------------------------------------------------------
