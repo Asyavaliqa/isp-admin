6b38d7c7f64996aca65cf95c427ef0fe8ffcec42
Ashary Vermaysha <asharyver13@gmail.com>
2023-02-01 18:13:00 +0700

    separate table of data for reusable

--- resources/views/pages/admin/client/index.blade.php
+++ resources/views/pages/admin/client/index.blade.php
@@ -5,65 +5,24 @@
 @endsection
 
 @section('content')
-<div class="container-lg">
-    <div class="row g-0">
-        <div class="card">
-            <div class="card-header">
-                <strong>Pelanggan</strong>
-            </div>
-            <div class="card-body py-4">
-                <div class="d-flex justify-content-between mb-3">
-                    <div class="px-3">
-                        <input type="text" class="form-control" placeholder="Search ..">
-                    </div>
+    <div class="container-lg">
+        <div class="row g-0 mb-4">
+            <div class="card">
+                <div class="card-header">
+                    <strong>Pelanggan</strong>
                 </div>
-                <div class="table-responsive px-3">
-                    <table class="table table-hover align-middle custom-table">
-                        <thead>
-                            <tr>
-                              <th scope="col">Nama Pelanggan</th>
-                              <th scope="col">Reseller</th>
-                              <th scope="col">No.Telp</th>
-                              <th scope="col">Status PPN</th>
-                              <th scope="col">Paket </th>
-                            </tr>
-                          </thead>
-                          <tbody class="table-group-divider">
-                           @foreach ($clients as $client)
-                               <tr>
-                                    <th>
-                                        <a href="">
-                                            <img alt="{{ $client->user->fullname }}" src="{{ asset($client->user->photo ?? 'assets/brand/GMDP_100x100.png') }}" class="img-thumbnail rounded-circle" style="width: 60px">
-                                            <span class="ms-2">{{ $client->user->fullname }}</span>
-                                        </a>
-                                    </th>
-                                    <td>
-                                        <a href="">
-                                            {{ $client->reseller->name }}
-                                        </a>
-                                    </td>
-                                    <td>
-                                        {{ $client->user->phone_number }}
-                                    </td>
-                                    <td>
-                                        @if ($client->is_ppn)
-                                        <span class="badge rounded-pill bg-primary">Ya</span>
-                                        @else
-                                        <span class="badge rounded-pill bg-danger">Tidak</span>
-                                        @endif
-                                    </td>
-                                    <td>
-                                        {{ $client->bandwidth->name }}
-                                    </td>
-                               </tr>
-                           @endforeach
-                          </tbody>
-                    </table>
-
+                <div class="card-body py-4">
+                    <div class="d-flex justify-content-between mb-3">
+                        <div class="px-3">
+                            <input type="text" class="form-control" placeholder="Search ..">
+                        </div>
+                    </div>
+                    <div class="table-responsive px-3">
+                        @include('pages.admin.client.indexTable')
+                    </div>
                     {{ $clients->links('components.pagination') }}
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
