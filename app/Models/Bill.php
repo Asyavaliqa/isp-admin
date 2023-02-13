78c1e4b16aa0c356cd31144d7a24d1a6775cd0c1
Ashary Vermaysha <asharyver13@gmail.com>
2023-02-13 08:52:46 +0700

    create and append accessor

--- app/Models/Bill.php
+++ app/Models/Bill.php
@@ -2,9 +2,11 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Carbon;
 
 /**
  * @mixin IdeHelperBill
@@ -35,6 +37,22 @@ class Bill extends Model
     const TYPE_UPGRADE = 3;
 
     /**
+     * The accessors to append to the model's array form.
+     *
+     * @var array
+     */
+    protected $appends = ['balance_formated', 'created_at_formated', 'payment_month_formated'];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'payment_month' => 'date',
+    ];
+
+    /**
      * Relation to reseller
      *
      * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
@@ -62,5 +80,52 @@ class Bill extends Model
     public function plan()
     {
         return $this->belongsTo(Plan::class);
+    }
+
+    /**
+     * Format price
+     *
+     * @return Illuminate\Database\Eloquent\Casts\Attribute
+     */
+    protected function balanceFormated(): Attribute
+    {
+        return Attribute::make(
+            function ($value, $attributes) {
+                return sprintf(
+                    'Rp%s',
+                    number_format($attributes['balance'], 2, ',', '.')
+                );
+            }
+        );
+    }
+
+    /**
+     * Format creation datetime
+     *
+     * @return Illuminate\Database\Eloquent\Casts\Attribute
+     */
+    protected function createdAtFormated(): Attribute
+    {
+        return Attribute::make(
+            function ($value, $attributes) {
+                return Carbon::parse($attributes['created_at'])
+                    ->isoFormat('dddd, D MMMM g');
+            }
+        );
+    }
+
+    /**
+     * Format payment for month datetime
+     *
+     * @return Illuminate\Database\Eloquent\Casts\Attribute
+     */
+    protected function paymentMonthFormated(): Attribute
+    {
+        return Attribute::make(
+            function ($value, $attributes) {
+                return Carbon::parse($attributes['payment_month'])
+                    ->isoFormat('MMMM g');
+            }
+        );
     }
 }
