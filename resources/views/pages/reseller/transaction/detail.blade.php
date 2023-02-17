06c47b947531ef9d74e4c51d0e2a560cd0279047
Ashary Vermaysha <asharyver13@gmail.com>
2023-02-17 11:09:13 +0700

    redessign detail bill

--- resources/views/pages/reseller/transaction/detail.blade.php
+++ resources/views/pages/reseller/transaction/detail.blade.php
@@ -7,156 +7,161 @@
 @section('content')
 {{-- TODO: Data masih mengambil dari data master --}}
 <div class="container-lg">
-    <div class="row g-0 mb-4">
-        <div class="card mb-4">
-            <div class="card-header">
-                <strong>Detail Tagihan</strong>
-            </div>
-            <div class="card-body">
-                <div class="table-responsive">
-                    <table class="table table-hover">
-                        <tr>
-                            <th scope="col">Nomor Invoice</th>
-                            <td scope="col">:</td>
-                            <td scope="col">{{ $transaction->invoice_id }}</td>
-                        </tr>
-                        <tr>
-                            <th scope="col">Pembayaran Bulan</th>
-                            <td scope="col">:</td>
-                            <td scope="col">
-                                <span class="badge badge-pills bg-info">
-                                    {{ $transaction->created_at->subMonth()->isoFormat('MMMM g') }}
-                                </span>
-                            </td>
-                        </tr>
-                        <tr>
-                            <th scope="col">Tagihan dibuat pada</th>
-                            <td scope="col">:</td>
-                            <td scope="col">{{ $transaction->created_at->isoFormat('dddd, D MMMM g') }}</td>
-                        </tr>
-                        <tr>
-                            <th scope="col">Nama Pelanggan</th>
-                            <td scope="col">:</td>
-                            <td scope="col">{{ $transaction->client->user->fullname ?? $transaction->client_name }}</td>
-                        </tr>
-                        <tr>
-                            <th scope="col">Paket</th>
-                            <td scope="col">:</td>
-                            <td scope="col">{{ $transaction->plan->name ?? $transaction->plan_name }}</td>
-                        </tr>
-                        <tr>
-                            <th scope="col">Jumlah Bayar</th>
-                            <td scope="col">:</td>
-                            <td scope="col">{{ $transaction->grand_total_formated }}</td>
-                        </tr>
-                        @if ($transaction->payed_at)
-                        <tr>
-                            <th scope="col">Bukti Pembayaran</th>
-                            <td scope="col">:</td>
-                            <td scope="col">
-                                <button type="button" class="btn btn-sm btn-primary" data-coreui-toggle="modal" data-coreui-target="#billPhoto">
-                                    Tampilkan Bukti Bayar
-                                </button>
-                            </td>
-                        </tr>
-                        @endif
-                        <tr>
-                            <th scope="col">Status Tagihan</th>
-                            <td scope="col">:</td>
-                            <td scope="col">
-                                @if (empty($transaction->payed_at) && empty($transaction->accepted_at))
-                                    <span class="badge badge-pills bg-danger">Belum dibayar</span>
-                                @elseif ($transaction->payed_at && empty($transaction->accepted_at))
-                                    <span class="badge badge-pills bg-warning">Belum dikonfirmasi</span>
-                                @elseif ($transaction->payed_at && $transaction->accepted_at)
-                                <span class="badge badge-pills bg-success">Sudah diselesai</span>
-                                @endif
-                            </td>
-                        </tr>
-                    </table>
+    <div class="row mb-4">
+        <div class="col-md-6">
+            <div class="card mb-4">
+                <div class="card-header">
+                    <strong>Detail Tagihan</strong>
                 </div>
-            </div>
-        </div>
-        <div class="card mb-4">
-            <div class="card-header">
-                <strong>Rincian Pembayaran</strong>
-            </div>
-            <div class="card-body">
-                <div class="table-responsive">
-                    <table class="table table-hover">
-                        <tr>
-                            <th scope="col">Nilai sebelum PPN</th>
-                            <td scope="col">:</td>
-                            <td scope="col">Rp{{ number_format($transaction->amount, 2, ',', '.') }}</td>
-                        </tr>
-                        <tr>
-                            <th scope="col">Nilai PPN</th>
-                            <td scope="col">:</td>
-                            <td scope="col">Rp{{ number_format($transaction->tax, 2, ',', '.') }}</td>
-                        </tr>
-                        <tr>
-                            <th scope="col">Nilai setelah Pajak/Nilai Invoice</th>
-                            <td scope="col">:</td>
-                            <td scope="col" class="fw-bold">Rp{{ number_format($transaction->grand_total, 2, ',', '.') }}</td>
-                        </tr>
-                    </table>
-                </div>
-            </div>
-        </div>
-        <div class="card mb-4">
-            <div class="card-header">
-                <strong>Paket</strong>
-            </div>
-            <div class="card-body">
-                <div class="table-responsive">
-                    <table class="table table-hover">
-                        <tr>
-                            <th scope="col">Nama Paket</th>
-                            <td scope="col">:</td>
-                            <td scope="col">{{ $transaction->plan_name }}</td>
-                        </tr>
-                        <tr>
-                            <th scope="col">Bandwidth</th>
-                            <td scope="col">:</td>
-                            <td scope="col">
-                                <span class="badge rounded-pill bg-primary">
-                                    {{ $transaction->plan_bandwidth }} Mbps
-                                </span>
-                            </td>
-                        </tr>
-                        <tr>
-                            <th scope="col">Harga</th>
-                            <td scope="col">:</td>
-                            <td scope="col">Rp{{ number_format($transaction->plan_price, 2, ',', '.') }}</td>
-                        </tr>
-                    </table>
-                </div>
-            </div>
-        </div>
-        <div class="card">
-            <div class="card-header">
-                <strong>Pelanggan</strong>
-            </div>
-            <div class="card-body">
-                <div class="row">
+                <div class="card-body">
                     <div class="table-responsive">
                         <table class="table table-hover">
                             <tr>
-                                <th scope="col">Nama</th>
+                                <th scope="col">Nomor Invoice</th>
                                 <td scope="col">:</td>
-                                <td scope="col">{{ $transaction->client->user->fullname }}</td>
+                                <td scope="col">{{ $transaction->invoice_id }}</td>
                             </tr>
                             <tr>
-                                <th scope="col">Alamat</th>
+                                <th scope="col">Pembayaran Bulan</th>
                                 <td scope="col">:</td>
-                                <td scope="col">{{ $transaction->client->user->address }}</td>
+                                <td scope="col">
+                                    <span class="badge badge-pills bg-info">
+                                        {{ $transaction->created_at->subMonth()->isoFormat('MMMM g') }}
+                                    </span>
+                                </td>
                             </tr>
                             <tr>
-                                <th scope="col">Nomor Telepon</th>
+                                <th scope="col">Tagihan dibuat pada</th>
                                 <td scope="col">:</td>
-                                <td scope="col">{{ $transaction->client->user->phone_number }}</td>
+                                <td scope="col">{{ $transaction->created_at->isoFormat('dddd, D MMMM g') }}</td>
+                            </tr>
+                            <tr>
+                                <th scope="col">Nama Pelanggan</th>
+                                <td scope="col">:</td>
+                                <td scope="col">{{ $transaction->client->user->fullname ?? $transaction->client_name }}</td>
+                            </tr>
+                            <tr>
+                                <th scope="col">Paket</th>
+                                <td scope="col">:</td>
+                                <td scope="col">{{ $transaction->plan->name ?? $transaction->plan_name }}</td>
+                            </tr>
+                            <tr>
+                                <th scope="col">Jumlah Bayar</th>
+                                <td scope="col">:</td>
+                                <td scope="col">{{ $transaction->grand_total_formated }}</td>
+                            </tr>
+                            <tr>
+                                <th scope="col">Status Tagihan</th>
+                                <td scope="col">:</td>
+                                <td scope="col">
+                                    @if (empty($transaction->payed_at) && empty($transaction->accepted_at))
+                                        <span class="badge badge-pills bg-danger">Belum dibayar</span>
+                                    @elseif ($transaction->payed_at && empty($transaction->accepted_at))
+                                        <span class="badge badge-pills bg-warning">Belum dikonfirmasi</span>
+                                    @elseif ($transaction->payed_at && $transaction->accepted_at)
+                                    <span class="badge badge-pills bg-success">Sudah diselesai</span>
+                                    @endif
+                                </td>
                             </tr>
                         </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="card mb-4">
+                <div class="card-header">
+                    <strong>Rincian Pembayaran</strong>
+                </div>
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table table-hover">
+                            <tr>
+                                <th scope="col">Nilai sebelum PPN</th>
+                                <td scope="col">:</td>
+                                <td scope="col">Rp{{ number_format($transaction->amount, 2, ',', '.') }}</td>
+                            </tr>
+                            <tr>
+                                <th scope="col">Nilai PPN</th>
+                                <td scope="col">:</td>
+                                <td scope="col">Rp{{ number_format($transaction->tax, 2, ',', '.') }}</td>
+                            </tr>
+                            <tr>
+                                <th scope="col">Nilai setelah Pajak/Nilai Invoice</th>
+                                <td scope="col">:</td>
+                                <td scope="col" class="fw-bold">Rp{{ number_format($transaction->grand_total, 2, ',', '.') }}</td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+                @if ($transaction->payed_at)
+                <div class="card-footer d-flex justify-content-end">
+                    <button type="button" class="btn btn-sm btn-primary" data-coreui-toggle="modal" data-coreui-target="#billPhoto">
+                        Tampilkan Bukti Bayar
+                    </button>
+                </div>
+                @endif
+            </div>
+        </div>
+
+        <div class="col-md-6">
+            <div class="card mb-4">
+                <div class="card-header">
+                    <strong>Paket</strong>
+                </div>
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table table-hover">
+                            <tr>
+                                <th scope="col">Nama Paket</th>
+                                <td scope="col">:</td>
+                                <td scope="col">{{ $transaction->plan_name }}</td>
+                            </tr>
+                            <tr>
+                                <th scope="col">Bandwidth</th>
+                                <td scope="col">:</td>
+                                <td scope="col">
+                                    <span class="badge rounded-pill bg-primary">
+                                        {{ $transaction->plan_bandwidth }} Mbps
+                                    </span>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="col">Harga</th>
+                                <td scope="col">:</td>
+                                <td scope="col">Rp{{ number_format($transaction->plan_price, 2, ',', '.') }}</td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-header">
+                    <strong>Pelanggan</strong>
+                </div>
+                <div class="card-body">
+                    <div class="row">
+                        <div class="table-responsive">
+                            <table class="table table-hover">
+                                <tr>
+                                    <th scope="col">Nama</th>
+                                    <td scope="col">:</td>
+                                    <td scope="col">{{ $transaction->client->user->fullname }}</td>
+                                </tr>
+                                <tr>
+                                    <th scope="col">Alamat</th>
+                                    <td scope="col">:</td>
+                                    <td scope="col">{{ $transaction->client->user->address }}</td>
+                                </tr>
+                                <tr>
+                                    <th scope="col">Nomor Telepon</th>
+                                    <td scope="col">:</td>
+                                    <td scope="col">{{ $transaction->client->user->phone_number }}</td>
+                                </tr>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -176,7 +181,6 @@
             <img src="{{ asset($transaction->bill_photo ?? 'assets/img/bills/belum-bayar.webp') }}" alt="{{ $transaction->invoice_id }}" class="img-thumbnail img-fluid mx-auto d-block">
         </div>
         <div class="modal-footer">
-            {{-- TODO Buat Aksi untuk konfirmasi transaksi --}}
             <button type="button" class="btn btn-primary">Konfirmasi</button>
             <button type="button" class="btn btn-secondary" data-coreui-dismiss="modal">Close</button>
         </div>
